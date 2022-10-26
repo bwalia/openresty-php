b6b6093bc300d35aa8c45dd745d81762f3f3b24d
Balinder Walia <balinder.walia@gmail.com>
2022-10-26 13:39:44 +0200

    Openresty PHP dockerfile updated with openresty version 1.21.4.1 and 8.1-fpm-alpine3.16

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# -- Author: Balinder Walia --
+# -- Pushes the docker image : openresty119php74 into AWS ECR / or optionally into a given Docker registry --
+# -- $1 is mandatory image version, for example : latest --
+
+set -x
+
+# VERSION=$1
+# AWS_PROFILE_NAME=$2
+# REGION=$3
+
+# if [ -z ${VERSION} ];
+# then
+#   echo "openresty-php docker image version not set"
+#   exit 1
+# fi
+docker build -f devops/docker/Dockerfile -t openresty-php .
+docker tag openresty-php:latest registry.workstation.co.uk/openresty-php:latest
+docker push registry.workstation.co.uk/openresty-php:latest
