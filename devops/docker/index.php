4382f94b5f5d918d206b08f2b8490e4191663c60
Balinder Walia <balinder.walia@gmail.com>
2023-01-31 21:50:54 +0100

    php conf listen unix socket fixed

--- devops/docker/index.php
+++ devops/docker/index.php
@@ -2,5 +2,5 @@
 error_reporting(E_ALL); 
 ini_set('display_errors', 1);
 //phpinfo();
-echo "Openresty v1.19.9.1 and PHP v7.4 are alive!";
+echo "Let's get coding - Your Openresty v1.21.4.1, Lua 5 and PHP v8.1 are working!";
 ?>
