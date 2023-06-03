6aeb5db7e05ef49441d07462a921ed1a90978509
Balinder Walia <bwalia@tenthmatrix.co.uk>
2023-06-03 21:42:08 +0200

    Pipeline improved and auto push to docker

--- devops/docker/php/index.php
+++ devops/docker/php/index.php
@@ -2,5 +2,5 @@
 error_reporting(E_ALL); 
 ini_set('display_errors', 1);
 //phpinfo();
-echo "Let's get coding - Your Openresty v1.21.4.1, Lua 5 and PHP v8.1 are working!";
+echo "Let's get coding - Your Openresty v1.21.4.1 setup with Lua 5 and PHP v8.1 seems to be working!";
 ?>
